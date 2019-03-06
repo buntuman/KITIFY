84cab475fe184fe6af20e4ea28c58db05dfa4c16
buntuman <buntuman@LAPTOP-S10HEVBP.localdomain>
2019-03-05 20:06:54 -0500

    Added component.py file

---
+++ component.py
@@ -0,0 +1,11 @@
+'''
+@Author :Daniel Gitahi
+This document contains the definition of a 'component' object.
+'''
+
+class Component:
+    def __init__(self):
+        self.id = None
+        self.group_id = None
+
+    def
