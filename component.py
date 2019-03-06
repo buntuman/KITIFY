451b4d42d34c684bce01937fe2ba47ee07d7943d
buntuman <buntuman@yohancatalina.co>
2019-03-05 22:54:59 -0500

    defined the component constructor

--- component.py
+++ component.py
@@ -4,8 +4,17 @@ This document contains the definition of a 'component' object.
 '''
 
 class Component:
-    def __init__(self):
-        self.id = None
-        self.group_id = None
+    def __init__(self, serial = None,model = None, parentSerial = None):
+        self.serial = serial
+        self.model=model
+        self.parentSerial = parentSerial
 
-    def
+    '''
+        This returns true if the component has a parent serial assigned.
+        The presence of a parent serial means that the component phycally resides
+        inside another component. 
+    '''
+    def isInternalComponent(self):
+        return self.parentSerial == None
+
+
